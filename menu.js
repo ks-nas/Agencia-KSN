5f7509c0fd10aa3923953fe9d8d7ebf1f852b636
ks-nas <159466437+ks-nas@users.noreply.github.com>
2024-02-22 14:21:16 -0300

    Version for mobile with menu

---
+++ menu.js
@@ -0,0 +1,16 @@
+
+let btnMenu = document.getElementById('btn-menu-mobile')
+let menuMobile = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menuMobile.classList.add('abrir-menu')
+})
+
+menuMobile.addEventListener('click', ()=>{
+    menuMobile.classList.remove('abrir-menu')
+})
+overlay.addEventListener('click', ()=>{
+    menuMobile.classList.remove('abrir-menu')
+})
+
